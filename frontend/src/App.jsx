3deddcea2c8274768f681694aef2261339add249
Deepali Singh <133847806+deepalis21@users.noreply.github.com>
2024-10-01 18:00:49 +0530

    Fixed some errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,12 @@ import Cart from './pages/Cart';
 import SignIn from './pages/SignIn';
 import Login from './pages/Login';
 import Menp from './pages/Menp';
-import About from './pages/Aboutus';
-import Contact from './pages/Contactus';
+import Aboutus from './pages/Aboutus';
+import Contactus from './pages/Contactus';
 import Women from './pages/Women';
 import Womenp from './pages/Womenp';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar'
 import Home from './pages/Home';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -40,16 +41,16 @@ function App() {
               <Link to="/Menp">Men Premium</Link>
             </li>
             <li>
-              <Link to="/Women">Women Premium</Link>
+              <Link to="/Women">Women</Link>
             </li>
             <li>
               <Link to="/Womenp">Women Premium</Link>
             </li>
             <li>
-              <Link to="/Contactus">About</Link>
+              <Link to="/Contactus">Contactus</Link>
             </li>
             <li>
-              <Link to="/Aboutus">Contact</Link>
+              <Link to="/Aboutus">Aboutus</Link>
             </li>
           </ul>
         
@@ -64,8 +65,8 @@ function App() {
            <Route path="/Login" element={<Login />} />
            <Route path="/Menp" element={<Menp />} />
            <Route path="/Womenp" element={<Womenp />} />
-           <Route path="/Aboutus" element={<About />} />
-           <Route path="/Contactus" element={<Contact />} />
+           <Route path="/Aboutus" element={<Aboutus />} />
+           <Route path="/Contactus" element={<Contactus />} />
            <Route path="*" element={<h1>404 - Page Not Found</h1>} />
         </Routes>
         <Footer />
